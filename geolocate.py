d51e846f5882238db6468ef0286760e43bb21567
Rusne Sileryte <rusne.sileryte@gmail.com>
2020-09-30 09:40:22 +0200

    Add clean dataset test

--- geolocate.py
+++ geolocate.py
@@ -1,2 +1,5 @@
 # Automate geolocation
-# Validate locations (point in postcode polygon)
+
+
+# Add column to the previous dataframe e.g. ontdoener_location & it's coordinates in WKT, RD_new
+# Validate locations (point in postcode polygon) Spatial_data/Postcodegebied SHP
