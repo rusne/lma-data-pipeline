79f575a98d19046aaff65ff8866e231cb5f9e0f1
Vasilis <bouzasbasilis@hotmail.com>
2020-10-02 17:07:22 +0200

    start on filtering & logging

--- filtering.py
+++ filtering.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 # filter lines:
+# no year/month included
 # empty company name
 # invalid/empty postcodes --> unless it's not in the Netherlands
 # invalid/empty addresses --> streetname anything but text
@@ -15,10 +16,16 @@ logs how many erroneous data points have been filtered on which basis
 and returns a dataframe ready for the further analysis
 """
 
+import logging
+import sys
 import pandas as pd
 
 
 def run(dataframe):
+    """
+
+    :param dataframe:
+    """
 
     # selection of the columns we want to include in our analysis
     LMA_columns = ['Afvalstroomnummer', 'VerwerkingsmethodeCode',
@@ -30,8 +37,8 @@ def run(dataframe):
                    'Ontdoener', 'Ontdoener_Postcode', 'Ontdoener_Plaats',
                    'Ontdoener_Straat', 'Ontdoener_Huisnr', 'Ontdoener_Land',
                    # Herkomst
-                   'Herkomst_Postcode', 'Herkomst_Straat', 'Herkomst_Plaats',
-                   'Herkomst_Huisnr', 'Herkomst_Land',
+                   'Herkomst_Postcode', 'Herkomst_Straat',
+                   'Herkomst_Plaats', 'Herkomst_Huisnr', 'Herkomst_Land',
                    # # Afzender
                    # 'Afzender', 'Afzender_Postcode', 'Afzender_Straat',
                    # 'Afzender_Plaats', 'Afzender_Huisnummer',
@@ -49,16 +56,19 @@ def run(dataframe):
                    # 'Ontvanger_Plaats', 'Ontvanger_Huisnummer',
                    # Verwerker
                    'Verwerker', 'Verwerker_Postcode', 'Verwerker_Straat',
-                   'Verwerker_Plaats', 'Verwerker_Huisnummer', 'Verwerker_Land']
+                   'Verwerker_Plaats', 'Verwerker_Huisnummer']
 
-    roles = ['Ontdoener', 'Herkomst', 'Verwerker']
+    # filter columns from original dataframe
+    logging.info('Filter requested columns...')
+    try:
+        LMA = dataframe[LMA_columns]
+        logging.info('Original entries: {}'.format(len(LMA.index)))
+    except Exception as error:
+        logging.critical(error)
+        sys.exit(1)
 
-    LMA = dataframe[LMA_columns]
-    print('Original dataset length:', len(LMA.index), 'lines,',)
-
-    # --1-- filter empty fields
-    print('skipping empty fields:')
-
+    # filter empty fields
+    logging.info('Skip empty fields...')
     non_empty_fields = ['Afvalstroomnummer', 'VerwerkingsmethodeCode',
                         'EuralCode', 'MeldPeriodeJAAR', 'MeldPeriodeMAAND',
                         'Gewicht_KG', 'Aantal_vrachten',
@@ -80,50 +90,51 @@ def run(dataframe):
                         'Verwerker', 'Verwerker_Postcode']
 
     for field in non_empty_fields:
-
-        # log all empty fields before removing them
+        # count row where field is empty
         e = len(LMA.index) - LMA[field].count()
         if e > 0:
-            print('lines do not have a year specified and will be removed')
+            logging.warning('{} lines with no {}'.format(e, field))
 
-    print(len(LMA.index) - LMA['MeldPeriodeMAAND'].count(),)
-    print('lines do not have a month specified and will be removed')
+            # keep rows with non empty field
+            not_empty = LMA[field].notnull()
 
-    # Remove those data entries that have empty fields
-    LMA = LMA[LMA.MeldPeriodeJAAR.notnull()]
-    LMA = LMA[LMA.MeldPeriodeMAAND.notnull()]
+            # if no postcode, check if country is not Netherlands
+            # note: Verwerker is always in the Netherlands!
+            role = field.split('_')[0]
+            if 'Postcode' in field and role != 'Verwerker':
+                country = role + '_Land'
+                LMA = LMA[(not_empty) | (LMA[country] != 'Nederland')]
+            else:
+                LMA = LMA[not_empty]
 
+    logging.info('Final entries: {}'.format(len(LMA.index)))
 
-    # --2-- filter invalid fields
-
-    print(len(LMA.index) - LMA[LMA['Gewicht_KG'] < 1].count(),)
-    print('lines have specified 0 weight and will be removed')
-
-    print(len(LMA.index) - LMA[LMA['Aantal_vrachten'] < 1].count(),)
-    print('lines have specified 0 trips and will be removed')
-
-    for role in roles:
-
-        print(len(LMA.index) - LMA[len(LMA[role + '_Postcode']) < 6].count(),)
-        print('lines have invalid {0} postcode and will be removed'.format(role))
-
-
-    LMA = LMA[LMA[LMA['Gewicht_KG'] >= 1]
-    LMA = LMA[LMA[LMA['Aantal_vrachten'] >= 1]
-
-    for role in roles:
-
-        print(len(LMA.index) - LMA[len(LMA[role + '_Postcode']) < 6].count(),)
-        print('lines have invalid {0} postcode and will be removed'.format(role))
-
-
-
-
-
-
-if __name__ == '__main__':
-
-    # TEST DATAFRAME
-    dataframe = pd.read_excel('Testing_data/1_full_dataset.xlsx')
-
-    run(dataframe)
+    # print(len(LMA.index) - LMA['MeldPeriodeMAAND'].count(),)
+    # print('lines do not have a month specified and will be removed')
+#
+#     # Remove those data entries that have empty fields
+#     LMA = LMA[LMA.MeldPeriodeJAAR.notnull()]
+#     LMA = LMA[LMA.MeldPeriodeMAAND.notnull()]
+#
+#
+#     # --2-- filter invalid fields
+#
+#     print(len(LMA.index) - LMA[LMA['Gewicht_KG'] < 1].count(),)
+#     print('lines have specified 0 weight and will be removed')
+#
+#     print(len(LMA.index) - LMA[LMA['Aantal_vrachten'] < 1].count(),)
+#     print('lines have specified 0 trips and will be removed')
+#
+#     for role in roles:
+#
+#         print(len(LMA.index) - LMA[len(LMA[role + '_Postcode']) < 6].count(),)
+#         print('lines have invalid {0} postcode and will be removed'.format(role))
+#
+#
+#     LMA = LMA[LMA[LMA['Gewicht_KG'] >= 1]
+#     LMA = LMA[LMA[LMA['Aantal_vrachten'] >= 1]
+#
+#     for role in roles:
+#
+#         print(len(LMA.index) - LMA[len(LMA[role + '_Postcode']) < 6].count(),)
+#         print('lines have invalid {0} postcode and will be removed'.format(role))
--- main.py
+++ main.py
@@ -1,44 +1,60 @@
 #!/usr/bin/env python3
 
+import logging
+import sys
 import pandas as pd
 
-import filter.run
-import clean.run
-import enhance.run
-import classify.run
-import analyze.run
-import save.run
-
+import filtering
+# import clean.run
+# import enhance.run
+# import classify.run
+# import analyze.run
+# import save.run
 
 import warnings  # ignore unnecessary warnings
 warnings.simplefilter(action="ignore", category=FutureWarning)
 pd.options.mode.chained_assignment = None
 
-# TEST DATAFRAME
-dataframe = pd.read_excel('Testing_data/1_full_dataset.xlsx')
-
-# 2016-2020 DATAFRAME
-# dataframe = pd.read_excel('Private_data/1_full_dataset.xlsx')
 
 if __name__ == '__main__':
+    # logging: timestamp, warning level & message
+    logging.basicConfig(filename='full.log',  # file name
+                        filemode='w',  # overwrite
+                        level=logging.DEBUG,  # lowest warning level
+                        format='%(asctime)s [%(levelname)s]: %(message)s')  # message format
+
+    # start pipeline
+    logging.info('START PIPELINE...')
+
+    # TEST DATAFRAME
+    logging.info('LOAD DATASET...')
+    try:
+        dataframe = pd.read_excel('Testing_data/1_full_dataset.xlsx')
+    except Exception as error:
+        logging.critical(error)
+        sys.exit(1)
 
     # filter
-    filtered_df = filter.run(dataframe)
+    logging.info('FILTER DATASET...')
+    filtered_df = filtering.run(dataframe)
 
-    # clean
-    cleaned_df = clean.run(filtered_df)
+    # end pipeline
+    logging.info('END PIPELINE...')
 
-    # enhance
-    geolocated_df = enhance.run(cleaned_df)
-
-    # enhance
-    connected_df = enhance.run(geolocated_df)
-
-    # classify
-    classified_df = classify.run(connected_df)
-
-    # analyze (emission)
-    analyzed_df = analyze.run(classified_df)
-
-    # save
-    saved_df = save.run(analyzed_df)
+    # # clean
+    # cleaned_df = clean.run(filtered_df)
+    #
+    # # enhance
+    # geolocated_df = enhance.run(cleaned_df)
+    #
+    # # enhance
+    # connected_df = enhance.run(geolocated_df)
+    #
+    # # classify
+    # classified_df = classify.run(connected_df)
+    #
+    # # analyze (emission)
+    # analyzed_df = analyze.run(classified_df)
+    #
+    # # save
+    # saved_df = save.run(analyzed_df)
